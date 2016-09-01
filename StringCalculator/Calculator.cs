3f57bb2a762fd87bebbf26522d26417e39d7a97e
pavel-yermalovich <x48ray@gmail.com>
2016-09-01 22:09:58 +0300

    first commit

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        private readonly List<string> _delimiters = new List<string> { ",", "\n" }; 
+        
+        public int Add(string input)
+        {
+            if (input == string.Empty)
+                return 0;
+
+            const string delimiterPrefix = "//";
+
+            if (input.StartsWith(delimiterPrefix))
+            {
+                ParseDelimiters(input, delimiterPrefix);
+                input = input.Substring(input.IndexOf('\n') + 1);
+            }
+
+            var numbers = input.Split(_delimiters.ToArray(), StringSplitOptions.None)
+                .Select(int.Parse)
+                .Where(n => n <= 1000).ToList();
+
+            ValidateNegatives(numbers);
+
+            return numbers.Sum();
+        }
+
+        private void ValidateNegatives(IEnumerable<int> numbers)
+        {
+            var negatives = numbers.Where(n => n < 0).ToList();
+            if (negatives.Any())
+                throw new ArgumentException("negatives not allowed: " + string.Join(", ", negatives));
+        }
+
+        private void ParseDelimiters(string input, string delimiterPrefix)
+        {
+            var regex = new Regex(@"\[(.*?)\]");
+            var matches = regex.Matches(input);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    _delimiters.Add(match.Groups[1].ToString());
+                }
+            }
+            else
+            {
+                _delimiters.Add(input[delimiterPrefix.Length].ToString());
+            }
+        }
+    }
+}
---
+++ Tests/CalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using StringCalculator;
+
+namespace Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        public Calculator Calculator = new Calculator();
+
+        [TestCase("", 0)]
+        [TestCase("1", 1)]
+        [TestCase("1,2,3", 6)]
+        [TestCase("1\n2\n3", 6)]
+        [TestCase("//x\n1x2x3", 6)]
+        [TestCase("//[xyz][sw][abb]\n1xyz2sw3abb0", 6)]
+        [TestCase("1,2,3,3000", 6)]
+        public void Add_Should_Return_Expected_Result(string input, int expected)
+        {
+            AddAndAssert(input, expected);
+        }
+
+        [TestCase("1,2,-3", "negatives not allowed: -3")]
+        [TestCase("1,2,-3,-4", "negatives not allowed: -3, -4")]
+        public void Negatives_Should_Troughs_Exception(string input, string expectedMessage)
+        {
+            TestDelegate test = () => Calculator.Add(input);
+
+            var exception = Assert.Throws<ArgumentException>(test);
+            Assert.IsNotNull(exception);
+            Assert.That(exception.Message, Is.EqualTo(expectedMessage));
+        }
+
+        private void AddAndAssert(string input, int expected)
+        {
+            int result = Calculator.Add(input);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
